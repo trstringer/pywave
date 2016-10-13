650704adf259a509c6e7e9ead41e1fe719269c9c
Thomas Stringer <tr.stringer@outlook.com>
2016-10-12 22:07:35 -0400

    (wip) refactor data retrieval

--- wave-scraper.py
+++ wave-scraper.py
@@ -5,33 +5,38 @@ import sys
 from bs4 import BeautifulSoup
 import requests
 
-def parse_table_data(soup, label_pattern):
-    pattern = re.compile(label_pattern)
-    element = soup.find(text=pattern)
+class SwellData():
+    def __init__(self, station_id):
+        self.station_id = station_id
 
-    if element is None:
-        return None
+    def retrieve_station_data(self):
+        req = requests.get('http://www.ndbc.noaa.gov/station_page.php?station={}'.format(self.station_id))
 
-    next_element = element.next
+        if req.status_code != 200:
+            print('Error fetching page! {}'.format(req.status_code))
+            sys.exit(1)
 
-    if next_element is None:
-        return None
+        soup = BeautifulSoup(req.text, 'lxml')
 
-    return str(re.sub(r'<(/)?td>', '', str(next_element))).strip()
+        data_to_parse = [r'Swell Height', r'Swell Period', r'Swell Direction']
+        for label in data_to_parse:
+            print(label, end=' :: ')
+            print(parse_table_data(soup, label))
 
-def retrieve_station_data(station_id):
-    req = requests.get('http://www.ndbc.noaa.gov/station_page.php?station={}'.format(station_id))
+    def parse_table_data(self, soup, label_pattern):
+        pattern = re.compile(label_pattern)
+        element = soup.find(text=pattern)
 
-    if req.status_code != 200:
-        print('Error fetching page! {}'.format(req.status_code))
-        sys.exit(1)
+        if element is None:
+            return None
 
-    soup = BeautifulSoup(req.text, 'lxml')
+        next_element = element.next
 
-    data_to_parse = [r'Swell Height', r'Swell Period', r'Swell Direction']
-    for label in data_to_parse:
-        print(label, end=' :: ')
-        print(parse_table_data(soup, label))
+        if next_element is None:
+            return None
+
+        return str(re.sub(r'<(/)?td>', '', str(next_element))).strip()
+
         
 def main(station_id):
     retrieve_station_data(station_id)
